954e07aac91a794421c1578ebdcdbe10887c85f0
temmchikf <fedoseev694@gmail.com>
2021-09-16 00:40:10 +0300

    sort

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,11 +8,32 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
-  }
+  public static void sort (int arr[]) {
+      int first = 0;
+      int last = arr.length - 1;
+      realSort(arr, first, last);
+    }
 
-  public static void sort (List<Integer> list) {
+    public static void realSort(int[] arr, int first, int last) {
+      if (first < last) {
+        int left = first, right = last, middle = arr[(left + right) / 2];
+        while (left < right) {
+          while (arr[left] < middle) left++;
+          while (arr[right] > middle) right--;
+          if (left <= right) {
+            int tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+            left++;
+            right--;
+          }
+        }
+        realSort(arr, first, right);
+        realSort(arr, left, last);
+      }
+    }
+
+ /* public static void sort (List<Integer> list) {
     Collections.sort(list);
-  }
+  } */
 }
